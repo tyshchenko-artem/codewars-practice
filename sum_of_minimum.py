82172a229dd4b1e7ee9a52b65ba6ac2152482293
tishchenko-a <a.tishchenko.la@gmail.com>
2021-03-08 19:46:39 +0200

    add sum_of_minimum.py

---
+++ sum_of_minimum.py
@@ -0,0 +1,9 @@
+def sum_of_minimums(numbers):
+    final_list = []
+    for el in numbers:
+        final_list.append(min(el))
+    return sum(final_list)
+
+check = sum_of_minimums([ [ 7,9,8,6,2 ], [6,3,5,4,3], [5,8,7,4,5] ])
+check = sum_of_minimums([ [11,12,14,54], [67,89,90,56], [7,9,4,3], [9,8,6,7] ])
+print(check)
